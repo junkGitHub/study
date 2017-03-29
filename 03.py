b885af2589d7ce84766db04f43f2aa4fc2921491
junkGitHub <junk.meado@ga3.so-net.ne.jp>
2017-03-30 02:51:40 +0900

    03
    start

---
+++ 03.py
@@ -0,0 +1,22 @@
+#coding: Shift_JIS
+import numpy as np
+from sklearn import datasets
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import accuracy_score
+from sklearn.linear_model import LogisticRegression
+from sklearn.linear_model import Perceptron
+from sklearn.svm import SVC
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.tree import export_graphviz
+from matplotlib.colors import ListedColormap
+import matplotlib.pyplot as plt
+
+# for sklearn 0.18's alternative syntax
+from distutils.version import LooseVersion as Version
+from sklearn import __version__ as sklearn_version
+if Version(sklearn_version) < '0.18':
+    from sklearn.grid_search import train_test_split
+else:
+    from sklearn.model_selection import train_test_split
